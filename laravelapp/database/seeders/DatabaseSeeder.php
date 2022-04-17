ca3f9bc78f2a841be9e2b42e82a2e986a3e83943
Илья Клементьев <i.klementiev@online-samsung.ru>
2022-04-17 18:06:26 +0300

    Добавлена фабрика для сущности Товар-Категория и генерация с учетом условия задача (у товара может быть от двух до 10 категорий)

--- laravelapp/database/seeders/DatabaseSeeder.php
+++ laravelapp/database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\Category;
 use App\Models\Product;
+use App\Models\ProductCategory;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -19,6 +20,20 @@ class DatabaseSeeder extends Seeder
 
 //        Product::factory(20)->create();
 
-        Category::factory(10)->create();
+//        Category::factory(10)->create();
+
+        $products = Product::all();
+
+        foreach ($products as $product) {
+            $categories = Category::all()->random(rand(2,10));
+            foreach ($categories as $category) {
+                ProductCategory::factory(1)->create([
+                    'product_id' => $product->id,
+                    'category_id' => $category->id
+                ]);
+            }
+        }
+
+
     }
 }
