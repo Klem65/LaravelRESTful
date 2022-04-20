0dda611e7bb49bce0bbbe3e162c39680d21295ec
Илья Клементьев <i.klementiev@online-samsung.ru>
2022-04-21 01:19:03 +0300

    Добавил seed

--- laravelapp/database/seeders/DatabaseSeeder.php
+++ laravelapp/database/seeders/DatabaseSeeder.php
@@ -18,9 +18,9 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-//        Product::factory(20)->create();
+        Product::factory(20)->create();
 
-//        Category::factory(10)->create();
+        Category::factory(10)->create();
 
         $products = Product::all();
 
